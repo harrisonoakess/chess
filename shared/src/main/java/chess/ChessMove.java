a8b094133a2b1cd56b8eb8101561ec3f6c0c1f7e
harrisonoakess <oakeshjo00@gmail.com>
2025-01-21 23:25:40 -0700

    ChessBoardTests, ChessPositionTests, and ChessMoveTests are now passing

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class ChessMove {
     private final ChessPosition startPosition;
     private final ChessPosition endPosition;
-    private ChessPiece.PieceType promotionPiece;
+    private final ChessPiece.PieceType promotionPiece;
 
     @Override
     public boolean equals(Object o) {
@@ -31,6 +31,7 @@ public class ChessMove {
                      ChessPiece.PieceType promotionPiece) {
         this.startPosition = startPosition;
         this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
