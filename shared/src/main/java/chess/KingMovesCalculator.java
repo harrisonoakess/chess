f23ea318b32d182a485fc1aa6f2dbd6aaf4de42e
harrisonoakess <oakeshjo00@gmail.com>
2025-01-23 17:57:39 -0700

    saving progress just in case

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -26,6 +26,14 @@ public class KingMovesCalculator implements PieceMovesCalculator {
             // creates a new position bases off optional moves
             ChessPosition pieceNewPosition =  new ChessPosition(positionalRowMoves, positionalColMoves);
 
+            // checks to see if it's on the board
+             if (pieceNewPosition.getRow() < 8 && pieceNewPosition.getColumn() < 8){
+                 // Check to see if the spot is empty or has an enemy piece
+//                 if (ChessPosition(positionalRowMoves, position_col) == null || ChessGame.TeamColor){
+//
+//                 }
+             }
+
             // Look on "ChessMove.java" line 30 for this. Adds the move to the array
             validMoves.add(new ChessMove(myPosition, pieceNewPosition, null));
 
