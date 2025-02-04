ac5537d4cf37d9f93fe5cf461f9bcfebe28fd092
harrisonoakess <oakeshjo00@gmail.com>
2025-02-04 13:39:40 -0700

    added the easier methods.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,20 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private TeamColor playerTurn;
+    private ChessBoard board;
+
     public ChessGame() {
+        this.playerTurn = TeamColor.WHITE;
 
     }
+
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return playerTurn;
     }
 
     /**
@@ -27,7 +32,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        playerTurn = team;
     }
 
     /**
@@ -46,7 +51,8 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece currentPiece = board.getPiece(startPosition);
+
     }
 
     /**
@@ -96,7 +102,8 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+
+        this.board = board;
     }
 
     /**
@@ -105,6 +112,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
