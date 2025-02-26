f86950aa9610a56330e2f8bfd56eb63df7096fdb
harrisonoakess <oakeshjo00@gmail.com>
2025-02-26 11:59:10 -0700

    Added spark for create clear, and create new user, but clear new user isnt done yet

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,10 @@
 package server;
 import dataaccess.datastorage.MemoryUserDAO;
+import model.UserData;
 import spark.*;
 
 public class Server {
-    private final MemoryUserDAO userDAO = new MemoryUserDAO(); // Example DAO instance
+    private final MemoryUserDAO userDAO = new MemoryUserDAO();
 
 
     public int run(int desiredPort) {
@@ -12,7 +13,17 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (request, response) -> {
+            userDAO.createNewUser(new UserData("test_user", "test_pass",""));
+            response.status(200);
+            return "{}"; // needs to be a json
+        });
 
+        Spark.delete("/db", (request, response) -> {
+            userDAO.ClearUsers();
+            response.status(200);
+            return "{}"; // needs to be a json
+        });
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
