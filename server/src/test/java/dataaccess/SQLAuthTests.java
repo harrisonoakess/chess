70157528e373a3033e9c98bd2976ebbb5ddc49bb
harrisonoakess <oakeshjo00@gmail.com>
2025-03-11 20:23:41 -0600

    added my tests to the correct folder so the autograder can see them

---
+++ server/src/test/java/dataaccess/SQLAuthTests.java
@@ -0,0 +1,56 @@
+package dataaccess;
+
+import dataaccess.DataAccessException;
+import dataaccess.datastorage.DBAuthDAO;
+import dataaccess.datastorage.DBUserDAO;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SQLAuthTests {
+    private DBAuthDAO authentication;
+
+    @BeforeEach
+    public void testReset(){
+        authentication = new DBAuthDAO();
+
+    }
+
+    @Test
+    @DisplayName("Create new auth token")
+    public void testCreateUserAuth() throws DataAccessException, SQLException {
+        AuthData authData = authentication.createUserAuth("fake_username");
+
+        Assertions.assertEquals("fake_username", authData.username());
+        Assertions.assertNotNull(authData.authToken());
+        Assertions.assertFalse(authData.authToken().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Delete auth token")
+    public void testDeleteUserAuth() throws DataAccessException, SQLException {
+        AuthData authData = authentication.createUserAuth("fake_username");
+        authentication.deleteUserAuth(authData.authToken());
+
+        Assertions.assertFalse(authentication.checkUserAuth(authData.authToken()));
+    }
+
+    @Test
+    @DisplayName("auth token does not exist")
+    public void testAuthNotFound() throws Exception{
+        AuthData authData = authentication.createUserAuth("fake_username");
+        Exception exception = assertThrows(Exception.class, () -> {
+            authentication.deleteUserAuth("fake auth");
+        });
+
+        Assertions.assertEquals("User not authenticated", exception.getMessage());
+    }
+}
---
+++ server/src/test/java/dataaccess/SQLUserTests.java
@@ -0,0 +1,117 @@
+package dataaccess;
+
+import dataaccess.DataAccessException;
+import dataaccess.datastorage.DBAuthDAO;
+import dataaccess.datastorage.DBUserDAO;
+import model.LoginRequest;
+import model.LoginResult;
+import model.RegisterRequest;
+import model.RegisterResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQLUserTests {
+    private UserService userService;
+    private DBAuthDAO authDAO;
+
+    @BeforeEach
+    public void testReset() throws SQLException, DataAccessException {
+        authDAO = new DBAuthDAO();
+        DBUserDAO memoryUserDAO = new DBUserDAO(authDAO);
+        userService = new UserService(memoryUserDAO, authDAO);
+        userService.clearData();
+
+    }
+
+    @Test
+    @DisplayName("Account added successfully")
+    public void testRegisterSuccess() throws DataAccessException, SQLException {
+        RegisterRequest request = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        RegisterResult result = userService.register(request);
+
+        Assertions.assertEquals("fake_username", result.username());
+    }
+    @Test
+    @DisplayName("User already exists")
+    public void testUserAlreadyExists() throws DataAccessException, SQLException {
+        RegisterRequest request = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        userService.register(request);
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            userService.register(request);
+        });
+        assertEquals("Error: User already exists", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Password left blank")
+    public void testBlankPassword() throws DataAccessException{
+        RegisterRequest request = new RegisterRequest("fake_username", null, "email@fake.gov");
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            userService.register(request);
+        });
+        assertEquals("Password cannot be blank", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Successful login")
+    public void testSuccessfullyLogin() throws DataAccessException, SQLException {
+        RegisterRequest registerRequest = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        userService.register(registerRequest);
+
+        LoginRequest loginRequest = new LoginRequest("fake_username", "fake_password");
+        LoginResult loginResult = userService.login(loginRequest);
+
+        assertEquals(loginRequest.username(), loginResult.username());
+    }
+    @Test
+    @DisplayName("User does not exist")
+    public void testUsernameNotFound() throws DataAccessException, SQLException {
+        RegisterRequest registerRequest = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        userService.register(registerRequest);
+
+        LoginRequest loginRequest = new LoginRequest("fake_username_", "fake_password");
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            userService.login(loginRequest);
+        });
+        assertEquals("User does not exist", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Incorrect password")
+    public void testIncorrectPassword() throws DataAccessException, SQLException {
+        RegisterRequest registerRequest = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        userService.register(registerRequest);
+
+        LoginRequest loginRequest = new LoginRequest("fake_username", "fake_password_");
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            userService.login(loginRequest);
+        });
+        assertEquals("Password does not match", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("successful logout")
+    public void testLogout() throws DataAccessException, SQLException {
+        RegisterRequest registerRequest = new RegisterRequest("fake_username", "fake_password", "email@fake.gov");
+        userService.register(registerRequest);
+
+        LoginRequest loginRequest = new LoginRequest("fake_username", "fake_password");
+        LoginResult loginResult = userService.login(loginRequest);
+
+        userService.logout(loginResult.authToken());
+        assertFalse(authDAO.checkUserAuth(loginResult.authToken()));
+    }
+
+
+}
