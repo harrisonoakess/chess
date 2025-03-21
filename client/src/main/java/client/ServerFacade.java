344c6440c5064f509d66c29523ca53b0c90c80fc
harrisonoakess <oakeshjo00@gmail.com>
2025-03-21 00:08:46 -0600

    added some of the functions from petshop to connect my backend to my frontend

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,76 @@
+package client;
+
+import exception.ResponseException;
+import model.RegisterRequest;
+import model.RegisterResult;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.*;
+
+
+public class ServerFacade {
+    private final String serverUrl;
+
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    // Registered endpoint is returning a RegisterResult as
+    // a json object (username, authToken)
+    public RegisterResult register(String username, String password, String email) throws ResponseException {
+        var path = "/user";
+        // this is what will be sent into the endpoint
+        RegisterRequest request = new RegisterRequest(username, password, email);
+        return makeRequest("POST", path, request, RegisterResult.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+
+
+
+
+
+
+}
