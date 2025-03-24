4b26b4aa57e6823b46276e1195b946c2000e935e
harrisonoakess <oakeshjo00@gmail.com>
2025-03-24 10:17:18 -0600

    added all the user tests, now starting on game tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,8 @@
 package client;
 
+import exception.ResponseException;
+import model.LoginResult;
+import model.RegisterResult;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -7,11 +10,15 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    private static String serverUrl;
+    private ServerFacade facade;
+
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        serverUrl = "http://localhost:" + port;
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -20,10 +27,59 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void setUp() throws ResponseException {
+        facade = new ServerFacade(serverUrl);
+        facade.clear();
+    }
+
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("register success")
+    public void registerSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        Assertions.assertEquals("fakeuser", result.username());
     }
+
+    @Test
+    @DisplayName("register fail: duplicate account")
+    public void registerFailure() throws ResponseException {
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+            facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        });
+    }
+    @Test
+    @DisplayName("login success")
+    public void loginSuccess() throws ResponseException {
+        facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        LoginResult result = facade.login("fakeuser", "fakePass");
+        Assertions.assertEquals("fakeuser", result.username());
+    }
+    @Test
+    @DisplayName("login fail: wrong password")
+    public void loginFailure() throws ResponseException {
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+            facade.login("fakeuser", "wrongPass");
+        });
+    }
+    @Test
+    @DisplayName("logout success")
+    public void logoutSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        facade.logout(result.authToken());
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.createGame("wrongAuth", "newGame");
+        });
+    }
+    @Test
+    @DisplayName("logout fail: user not logged in")
+    public void logoutFailure() throws ResponseException {
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.logout("no auth");
+        });
+    }
+
 
 }
