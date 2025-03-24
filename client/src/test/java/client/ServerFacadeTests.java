89ba512c2a96ec4e34a7ca9af7375289c4e1d801
harrisonoakess <oakeshjo00@gmail.com>
2025-03-24 10:34:17 -0600

    facade tests are all done now

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,10 +1,14 @@
 package client;
 
 import exception.ResponseException;
+import model.CreateGameResult;
+import model.ListAllGamesResult;
 import model.LoginResult;
 import model.RegisterResult;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.awt.*;
 
 
 public class ServerFacadeTests {
@@ -80,6 +84,62 @@ public class ServerFacadeTests {
             facade.logout("no auth");
         });
     }
-
-
+    @Test
+    @DisplayName("create game success")
+    public void createGameSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        CreateGameResult gameResult = facade.createGame(result.authToken(), "newGame");
+        Assertions.assertNotNull(gameResult.gameID());
+    }
+    @Test
+    @DisplayName("create game fail: user not authorized")
+    public void createGameFail() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        Assertions.assertThrows(ResponseException.class, () -> {
+            CreateGameResult gameResult = facade.createGame("badAuth", "newGame");
+        });
+    }
+    @Test
+    @DisplayName("join game success")
+    public void joinGameSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        CreateGameResult gameResult = facade.createGame(result.authToken(), "newGame");
+        facade.joinGame("BLACK", String.valueOf(gameResult.gameID()), result.authToken());
+        ListAllGamesResult games = facade.listGames(result.authToken());
+        Assertions.assertEquals("fakeuser",games.games()[0].blackUsername());
+    }
+    @Test
+    @DisplayName("join game fail: invalid color")
+    public void joinGameFail() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        CreateGameResult gameResult = facade.createGame(result.authToken(), "newGame");
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.joinGame("RED", String.valueOf(gameResult.gameID()), result.authToken());
+        });
+    }
+    @Test
+    @DisplayName("list games success")
+    public void listGamesSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        CreateGameResult gameResult = facade.createGame(result.authToken(), "newGame");
+        ListAllGamesResult games = facade.listGames(result.authToken());
+        Assertions.assertEquals(1, games.games().length);
+    }
+    @Test
+    @DisplayName("list game fail: invalid auth")
+    public void listGamesFail() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        CreateGameResult gameResult = facade.createGame(result.authToken(), "newGame");
+        Assertions.assertThrows(ResponseException.class, () -> {
+            ListAllGamesResult games = facade.listGames("badAuth");
+        });
+    }
+    @Test
+    @DisplayName("clear data success")
+    public void clearSuccess() throws ResponseException {
+        RegisterResult result = facade.register("fakeuser", "fakePass", "fakeEmail@fake.org");
+        facade.clear();
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.login("fakeuser", "wrongPass");
+        });}
 }
